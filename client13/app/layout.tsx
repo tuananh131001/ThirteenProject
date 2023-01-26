d15e3e92e805578e0c9f1780ed49560916ccbc57
WilliamA1pha <s3864077@rmit.edu.vn>
2023-01-26 14:39:29 +0700

    Update set theme and perfect button change

--- client13/app/layout.tsx
+++ client13/app/layout.tsx
@@ -11,12 +11,9 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      
       <body className="antialiased max-w-4xl mb-40 flex flex-col md:flex-row mx-4 mt-8 md:mt-20 lg:mt-32 lg:mx-auto">
-      <SideBar />
         <Providers>
-          {" "}
-          <Container>{children} </Container>
+          <SideBar /> <Container>{children} </Container>
         </Providers>
       </body>
     </html>
--- client13/components/SideBar.tsx
+++ client13/components/SideBar.tsx
@@ -4,6 +4,8 @@ import clsx from "clsx";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 const navItems: any = {
   "/": {
@@ -32,48 +34,73 @@ const navItems: any = {
   },
 };
 
+function LetterA() {
+  return (
+    <motion.div
+      initial={{ scale: 0, rotate: 45 }}
+      animate={{ scale: 1, rotate: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="dark:text-white text-7xl font-bold shadow-sm text-orange-600">
+        A
+      </div>
+    </motion.div>
+  );
+}
+const letterStyle = {
+  fontSize: "75px",
+  fontWeight: "bold",
+  color: "#FFFFFF",
+  textShadow: "2px 2px #000",
+};
 function Logo() {
   return (
     <Link aria-label="Nguyen Tuan Anh" href="/">
-      <motion.svg
-        className="text-black dark:text-white h-[25px] md:h-[37px]"
-        width="25"
-        height="37"
-        viewBox="0 0 232 316"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <motion.path
-          initial={{
-            opacity: 0,
-            pathLength: 0,
-          }}
-          animate={{
-            opacity: 1,
-            pathLength: 1,
-          }}
-          transition={{
-            duration: 0.5,
-            type: "spring",
-            stiffness: 50,
-          }}
-          d="M39 316V0"
-          stroke="currentColor"
-          strokeWidth={78}
-        />
-        <motion.path
-          initial={{ x: -200, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{
-            duration: 0.5,
-            type: "spring",
-            stiffness: 50,
-          }}
-          d="M232 314.998H129.852L232 232.887V314.998Z"
-          fill="currentColor"
-        />
-      </motion.svg>
+      <LetterA />
     </Link>
+  );
+}
+function ToggleThemeButton() {
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
+
+  // After mounting, we have access to the theme
+  useEffect(() => setMounted(true), []);
+  return (
+    <button
+      aria-label="Toggle Dark Mode"
+      type="button"
+      className="  w-9 h-9 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center  hover:ring-2 ring-gray-300  transition-all"
+      onClick={() => {
+        setTheme(resolvedTheme === "dark" ? "light" : "dark");
+      }}
+    >
+      {mounted && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          className="w-5 h-5 text-gray-800 dark:text-gray-200"
+        >
+          {resolvedTheme === "dark" ? (
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+            />
+          ) : (
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+            />
+          )}
+        </svg>
+      )}
+    </button>
   );
 }
 
@@ -154,6 +181,10 @@ export default function SideBar() {
                 </Link>
               );
             })}
+            <div className="pl-2">
+              {" "}
+              <ToggleThemeButton />
+            </div>
           </div>
         </nav>
       </div>
