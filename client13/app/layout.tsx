88ba6f0af828e28a20b26b867537b3909567afaf
WilliamA1pha <s3864077@rmit.edu.vn>
2023-01-26 12:34:54 +0700

    New sidebar

--- client13/app/layout.tsx
+++ client13/app/layout.tsx
@@ -2,6 +2,7 @@
 import "./global.css";
 import Container from "@/components/Container";
 import { Providers } from "./providers";
+import SideBar from "@/components/SideBar";
 
 export default function RootLayout({
   children,
@@ -10,9 +11,9 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head></head>
-
-      <body>
+      
+      <body className="antialiased max-w-4xl mb-40 flex flex-col md:flex-row mx-4 mt-8 md:mt-20 lg:mt-32 lg:mx-auto">
+      <SideBar />
         <Providers>
           {" "}
           <Container>{children} </Container>
