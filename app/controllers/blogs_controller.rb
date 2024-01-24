dd266706777ba3be431c651ca9199adb122889d8
Anh Nguyen <anh.nt@josys.com>
2024-01-24 15:34:06 +0700

    Add blog

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,38 @@
+class BlogsController < ApplicationController
+  def create
+    @blog = Blog.new(blog_params)
+    @blog.save
+    redirect_to @blog
+  end
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+
+    if @blog.update(blog_params)
+      redirect_to @blog
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def delete
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+end
---
+++ spec/requests/blogs_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe "Blogs", type: :request do
+  describe "GET /create" do
+    it "returns http success" do
+      get "/blogs/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /index" do
+    it "returns http success" do
+      get "/blogs/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /update" do
+    it "returns http success" do
+      get "/blogs/update"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /delete" do
+    it "returns http success" do
+      get "/blogs/delete"
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
